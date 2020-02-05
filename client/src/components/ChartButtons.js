d2314fd1ca54e7fe47bc43bb13013641667d3ff0
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-05 18:05:34 -0500

    included group button component to show or hide data

---
+++ client/src/components/ChartButtons.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Button, ButtonToolbar } from 'react-bootstrap';
+import './ChartButtons.css';
+
+function ChartButtons(props) {
+
+  return (
+    <Container className="buttonContainer align-items-center">
+      <h5 className="title">Click to remove/add data</h5>
+      <ButtonToolbar className="justify-content-center button align-items-center">
+        <Button
+          className="buttonRevenue"
+          onClick={props.onClickRevenue}
+        >Revenue</Button>
+        <Button
+          className="buttonImpressions"
+          onClick={props.onClickImpressions}
+        >Impressions</Button>
+        <Button
+          className="buttonClicks"
+          onClick={props.onClickClicks}
+        >Clicks</Button>
+        <Button
+          className="buttonEvents"
+          onClick={props.onClickEvents}
+        >Events</Button>
+      </ButtonToolbar>
+    </Container>
+  )
+}
+
+export default ChartButtons;
