242c9b2a4c09421be704fbbed9b59b0f9af5119f
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-05 12:21:39 -0500

    included a Legend

--- client/src/components/GroupChart.js
+++ client/src/components/GroupChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { VictoryChart, VictoryGroup, VictoryBar, VictoryAxis, VictoryLabel } from 'victory';
+import { VictoryChart, VictoryGroup, VictoryBar, VictoryLegend, VictoryAxis, VictoryLabel } from 'victory';
 import moment from 'moment';
 
 export default function GroupChart(props) {
@@ -30,13 +30,13 @@ export default function GroupChart(props) {
     return moment(date).subtract(1, 'day').format('YYYY-MM-DD');
   }
 
-  let minDomain = {};
-  if(props.eventsDaily[0]) {
-    minDomain = substractDayfromDate(props.eventsDaily[0].date);
-    console.log(minDomain);
-  }
+  // let minDomain = {};
+  // if(props.eventsDaily[0]) {
+  //   minDomain = substractDayfromDate(props.eventsDaily[0].date);
+  //   console.log(minDomain);
+  // }
 
-  eventsDailyData.unshift({x: minDomain, y: 0});
+  // eventsDailyData.unshift({x: minDomain, y: 0});
 
   return (
     <main className="group_chart">
@@ -47,16 +47,26 @@ export default function GroupChart(props) {
           scale={{y: "log"}}
         />
         <VictoryAxis
-          label={'number of events'}
           dependentAxis={true}
           tickLabelComponent={<VictoryLabel style={{fontSize: '8px'}}/>}
           scale={{y: "log"}}
         />
+          <VictoryLegend x={40} y={10}
+            orientation="horizontal"
+            gutter={20}
+            colorScale={"blue"}
+            data={[
+              { name: "revenue", symbol: { type: "square"} },
+              { name: "impressions", symbol: { type: "square" } },
+              { name: "clicks", symbol: { type: "square"} },
+              { name: "events", fontSize: '4px', symbol: { type: "square" } },
+            ]}
+          />
         <VictoryGroup offset={10}
           colorScale={"blue"}
         >
           <VictoryBar
-            data={ eventsDailyData }
+            data={ revenueDailyData }
           />
           <VictoryBar
             data={ impressionsDailyData }
@@ -64,8 +74,8 @@ export default function GroupChart(props) {
           <VictoryBar
             data={ clicksDailyData }
           />
-            <VictoryBar
-            data={ revenueDailyData }
+          <VictoryBar
+            data={ eventsDailyData }
           />
         </VictoryGroup>
       </VictoryChart>
