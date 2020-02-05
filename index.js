ad78d3d3fd691283d6188edda363e1c7c022e437
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-04 19:17:26 -0500

    added path, ENV, and dotenv to get access to database config param

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const pg = require('pg')
+const ENV = require("./environment");
+const path = require("path");
+const PATH = path.resolve(__dirname, ".env." + ENV);
 
-const app = express()
+require("dotenv").config({ path: PATH });
+
+
+const app = express();
 // configs come from standard PostgreSQL env vars
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
-const pool = new pg.Pool()
+const pool = new pg.Pool();
 
 const queryHandler = (req, res, next) => {
   pool.query(req.sqlQuery).then((r) => {
