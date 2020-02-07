90b25034074a58e095031fe30c661f8be51c874b
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-07 18:17:40 -0500

    computed values for data to display and styled

--- client/src/components/HourlyDataTable.js
+++ client/src/components/HourlyDataTable.js
@@ -3,15 +3,51 @@ import MaterialTable, { MTableToolbar } from "material-table";
 
 function HourlyDataTable(props) {
 
+  console.log('props in HourlyDatatable: ', props);
+
+  const addDataForHour = (i, array, dataType) => {
+    let hourData = array.filter((elem) => {
+        return elem.hour === i;
+    });
+
+    let sumOfData = hourData.reduce((acc, curElem) => {
+        return acc + Number(curElem[dataType]);
+    }, 0);
+    //round up to 2 decimals
+    if(dataType === 'revenue') {
+      sumOfData = sumOfData.toFixed(2)
+    }
+    return sumOfData;
+  };
+
+  const createTableData = () => {
+
+    let finalArray = [];
+    for (let i = 0; i < 24; i++) {
+      finalArray[i] = {};
+      finalArray[i].hour = i;
+      // creates array of same hour objects
+      finalArray[i].impressions = addDataForHour(i, props.statsHourly, 'impressions');
+      finalArray[i].revenue = addDataForHour(i, props.statsHourly, 'revenue');
+      finalArray[i].clicks = addDataForHour(i, props.statsHourly, 'clicks');
+      finalArray[i].events = addDataForHour(i, props.eventsHourly, 'events');
+    }
+    return finalArray;
+  };
+
+  let dataList = createTableData();
+  console.log('dataList: ', dataList);
+
   return (
     <div style={{ maxWidth: "100%" }}>
       <MaterialTable
         components={{
           Toolbar: props => (
-            <div style = {{ color: "#940031",
-                            backgroundColor: '#e8eaf5',
-                            fontWeight: 'bold'
-                         }}
+            <div style =
+              {{ color: "mediumslateblue",
+                  backgroundColor: '#e8eaf5',
+                  fontWeight: 'bold'
+              }}
             >
               <MTableToolbar {...props}/>
             </div>
@@ -21,17 +57,17 @@ function HourlyDataTable(props) {
           pageSize: 10,
           search: true,
           padding: 'dense',
-          headerStyle:{ color :'#940031', fontWeight: 'bold' }
+          headerStyle:{ color :'mediumslateblue', fontWeight: 'bold' }
         }}
         columns={[
-          { title: "Hour", field: "date", cellStyle:{ padding: '10px'} },
+          { title: "Hour", field: "hour", cellStyle:{ padding: '10px'} },
           { title: "Impressions", field: "impressions" },
           { title: "Revenue", field: "revenue" },
           { title: "Clicks", field: "clicks" },
           { title: "Events", field: "events" },
         ]}
         data={dataList}
-        title="Daily Data"
+        title="Hourly Data"
       />
     </div>
   );
