a400d70585647a3fdfa4b977ee39680073588c06
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-07 17:31:48 -0500

    added axios calls to fetch data and passed state props to child components

--- client/src/components/DailyData.js
+++ client/src/components/DailyData.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
 import BarChart from './BarChart';
-import DataTable from './DataTable';
+import DailyDataTable from './DailyDataTable';
 
 
 function DailyData(props) {
 
+  const [state, setState] = useState({
+    eventsDaily: [],
+    statsDaily: [],
+    dataArrived: false
+  });
+
+  const axiosGet = (url) => {
+    return axios
+            .get(url)
+            .catch((error) => {
+              console.log(error.response.status);
+              console.log(error.response.headers);
+              console.log(error.response.data);
+            })
+  };
+
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(
+        axiosGet('http://localhost:5555/events/daily')
+      ),
+      Promise.resolve(
+        axiosGet('http://localhost:5555/stats/daily')
+      )
+    ]).then((all) => {
+        console.log('all: ', all);
+        setState(prev => ({
+          eventsDaily: all[0].data,
+          statsDaily: all[1].data,
+          dataArrived: true
+        }));
+    });
+  }, []);
+
   return (
     <Container>
       <Row>
-        <Col>
-          <BarChart
-            eventsDaily={ props.eventsDaily }
-            statsDaily={ props.statsDaily }
-          />
+        <Col lg={7}>
+          {state.dataArrived && <BarChart
+            eventsDaily={ state.eventsDaily }
+            statsDaily={ state.statsDaily }
+          />}
         </Col>
-        <Col>
-          <DataTable/>
+        <Col lg={5}>
+          {state.dataArrived && <DailyDataTable
+            eventsDaily={ state.eventsDaily }
+            statsDaily={ state.statsDaily }
+          />}
         </Col>
       </Row>
     </Container>
