e42bfea70ed75ffe15503b9f7d36fa1d2a5ce85e
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-07 21:48:31 -0500

    commented for now fetching data logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,54 +9,54 @@ import './App.css';
 
 function App() {
 
-  const [state, setState] = useState({
-    eventsHourly: [],
-    eventsDaily: [],
-    statsHourly: [],
-    statsDaily: [],
-    poi: [],
-    dataLoading: true
-  });
+  // const [state, setState] = useState({
+  //   eventsHourly: [],
+  //   eventsDaily: [],
+  //   statsHourly: [],
+  //   statsDaily: [],
+  //   poi: [],
+  //   dataLoading: true
+  // });
 
-  const axiosGet = (url) => {
-    return axios
-            .get(url)
-            .catch((error) => {
-              console.log(error.response.status);
-              console.log(error.response.headers);
-              console.log(error.response.data);
-            })
-  };
+  // const axiosGet = (url) => {
+  //   return axios
+  //           .get(url)
+  //           .catch((error) => {
+  //             console.log(error.response.status);
+  //             console.log(error.response.headers);
+  //             console.log(error.response.data);
+  //           })
+  // };
 
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(
-        axiosGet('http://localhost:5555/events/hourly')
-      ),
-      Promise.resolve(
-        axiosGet('http://localhost:5555/events/daily')
-      ),
-      Promise.resolve(
-        axiosGet('http://localhost:5555/stats/hourly')
-      ),
-      Promise.resolve(
-        axiosGet('http://localhost:5555/stats/daily')
-      ),
-      Promise.resolve(
-        axiosGet('http://localhost:5555/poi')
-      )
-    ]).then((all) => {
-        console.log('all: ', all);
-        setState(prev => ({
-          eventsHourly: all[0].data,
-          eventsDaily: all[1].data,
-          statsHourly: all[2].data,
-          statsDaily: all[3].data,
-          poi: all[4].data,
-          dataLoading: false
-        }));
-    });
-  }, []);
+  // useEffect(() => {
+  //   Promise.all([
+  //     Promise.resolve(
+  //       axiosGet('http://localhost:5555/events/hourly')
+  //     ),
+  //     Promise.resolve(
+  //       axiosGet('http://localhost:5555/events/daily')
+  //     ),
+  //     Promise.resolve(
+  //       axiosGet('http://localhost:5555/stats/hourly')
+  //     ),
+  //     Promise.resolve(
+  //       axiosGet('http://localhost:5555/stats/daily')
+  //     ),
+  //     Promise.resolve(
+  //       axiosGet('http://localhost:5555/poi')
+  //     )
+  //   ]).then((all) => {
+  //       console.log('all: ', all);
+  //       setState(prev => ({
+  //         eventsHourly: all[0].data,
+  //         eventsDaily: all[1].data,
+  //         statsHourly: all[2].data,
+  //         statsDaily: all[3].data,
+  //         poi: all[4].data,
+  //         dataLoading: false
+  //       }));
+  //   });
+  // }, []);
 
 
   // async const fetchData =() => {
@@ -98,17 +98,46 @@ function App() {
   // }
 
 
-  console.log('state: ', state);
+  // console.log('state: ', state);
 
-  return (
+//   return (
+//     <main className="App">
+//       <Navigation/>
+//       <Switch>
+//         <Route path="/dailydata"
+//                 render={ props => (
+//                     <DailyData
+//                       eventsDaily={ state.eventsDaily }
+//                       statsDaily={ state.statsDaily }
+//                     />
+
+//                 )}
+//         />
+//         <Route path="/hourlydata"
+//                 render={
+//                   props => (
+//                     <HourlyData
+//                       eventsDaily={ state.eventsHourly }
+//                       statsDaily={ state.statsHourly }
+//                     />
+//                   )
+//                 }
+//         />
+//         <Route path="/geodata" component={GeoMapData} />
+//         <Route component={""} />
+//       </Switch>
+//     </main>
+//   );
+// }
+
+return (
     <main className="App">
       <Navigation/>
       <Switch>
         <Route path="/dailydata"
                 render={ props => (
                     <DailyData
-                      eventsDaily={ state.eventsDaily }
-                      statsDaily={ state.statsDaily }
+
                     />
 
                 )}
@@ -117,8 +146,7 @@ function App() {
                 render={
                   props => (
                     <HourlyData
-                      eventsDaily={ state.eventsHourly }
-                      statsDaily={ state.statsHourly }
+
                     />
                   )
                 }
@@ -129,33 +157,5 @@ function App() {
     </main>
   );
 }
-
-// return (
-//     <main className="App">
-//       <Navigation/>
-//       <Switch>
-//         <Route path="/dailydata"
-//                 render={ props => (
-//                     <DailyData
-
-//                     />
-
-//                 )}
-//         />
-//         <Route path="/hourlydata"
-//                 render={
-//                   props => (
-//                     <HourlyData
-
-//                     />
-//                   )
-//                 }
-//         />
-//         <Route path="/geodata" component={""} />
-//         <Route component={""} />
-//       </Switch>
-//     </main>
-//   );
-// }
 
 export default App;
