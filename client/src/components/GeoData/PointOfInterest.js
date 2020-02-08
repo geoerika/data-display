0f98bcd07af52e991a67baf2c19d8484c9db95e1
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-08 11:12:50 -0500

    created a fontawesome marker for the map

---
+++ client/src/components/GeoData/PointOfInterest.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './PointOfInterest.css';
+
+function PointOfInterest() {
+
+  return (
+      <div>
+        <FontAwesomeIcon className="point-of-interest" icon="map-marker-alt" />
+      </div>
+    )
+}
+export default PointOfInterest;
