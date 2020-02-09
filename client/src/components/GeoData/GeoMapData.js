6857d474e738b4fac4332e4c915ffbc6ac463308
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-08 19:09:35 -0500

    cleaned component

--- client/src/components/GeoData/GeoMapData.js
+++ client/src/components/GeoData/GeoMapData.js
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import makePointOfInterestList from './helpers/makePointOfInterestList';
 import makePointDataList from './helpers/makePointDataList';
 
-function GeoMapData() {
+function GeoMapData(props) {
 
-  URL = process.env.REACT_APP_API_ENDPOINT;
   const GMAPAPIKEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
-
-  console.log('GMAPAPIKEY:',  GMAPAPIKEY);
 
   const [state, setState] = useState({
     center: {
@@ -17,47 +13,16 @@ function GeoMapData() {
       lng: -102.4960887
     },
     zoom: 4,
-    poi: [],
-    geoData: [],
-    dataArrived: false
+    poi: props.poi
   });
-
-  function axiosGet (url) {
-    return axios
-            .get(url)
-            .catch((error) => {
-              console.log(error.response.status);
-              console.log(error.response.headers);
-              console.log(error.response.data);
-            })
-  };
-
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(
-        axiosGet(`${URL}/poi`)
-      ),
-      Promise.resolve(
-        axiosGet(`${URL}/geo`)
-      )
-    ]).then((all) => {
-        console.log('all in GEO: ', all);
-        setState(prev => ({
-          ...state,
-          poi: all[0].data,
-          geoData: all[1].data,
-          dataArrived: true
-        }));
-    });
-  }, []);
 
 console.log('state: ', state);
 
   let locationList = makePointOfInterestList(state.poi);
-  let pointEventsList = makePointDataList(state.geoData, "events");
-  let pointImpressionsList = makePointDataList(state.geoData, "impressions");
-  let pointRevenueList = makePointDataList(state.geoData, "revenue");
-  let pointClicksList = makePointDataList(state.geoData, "clicks");
+  let pointEventsList = makePointDataList(state.poi, "events");
+  let pointImpressionsList = makePointDataList(state.poi, "impressions");
+  let pointRevenueList = makePointDataList(state.poi, "revenue");
+  let pointClicksList = makePointDataList(state.poi, "clicks");
 
   return (
     // Important! Always set the container height explicitly
