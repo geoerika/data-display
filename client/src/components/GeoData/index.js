1ac790b880800d24fcdcd656d11032a7b7a295b8
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-08 19:10:33 -0500

    moved database call here

--- client/src/components/GeoData/index.js
+++ client/src/components/GeoData/index.js
@@ -2,60 +2,86 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
 import Buttons from '../Buttons';
+import GeoDataTable from './GeoDataTable';
 import GeoMapData from './GeoMapData';
+import useHideAddData from '../../hooks/useHideAddData';
+import './index.css';
 
 function GeoData(props) {
 
-  // URL = process.env.REACT_APP_API_ENDPOINT;
-  // console.log('URL: ', URL);
+  const URL = process.env.REACT_APP_API_ENDPOINT;
 
-  // const [state, setState] = useState({
-  //   eventsDaily: [],
-  //   statsDaily: [],
-  //   dataArrived: false
-  // });
+  const [state, setState] = useState({
+    poi: [],
+    dataArrived: false
+  });
 
+  function axiosGet (url) {
+    return axios
+            .get(url)
+            .catch((error) => {
+              console.log(error.response.status);
+              console.log(error.response.headers);
+              console.log(error.response.data);
+            })
+  };
 
+  useEffect(() => {
+      Promise.resolve(
+        axiosGet(`${URL}/poi`)
+      )
+      .then(response => {
+        console.log('response in index: ', response);
+        setState(prev => ({
+          ...state,
+          poi: response.data,
+          dataArrived: true
+        }));
+      });
+  }, []);
 
-  // const axiosGet = (url) => {
-  //   return axios
-  //           .get(url)
-  //           .catch((error) => {
-  //             console.log(error.response.status);
-  //             console.log(error.response.headers);
-  //             console.log(error.response.data);
-  //           })
-  // };
+console.log('state: ', state);
 
-  // useEffect(() => {
-  //   Promise.all([
-  //     Promise.resolve(
-  //       axiosGet(`${URL}/events/daily`)
-  //     ),
-  //     Promise.resolve(
-  //       axiosGet(`${URL}/stats/daily`)
-  //     )
-  //   ]).then((all) => {
-  //       console.log('all: ', all);
-  //       setState(prev => ({
-  //         eventsDaily: all[0].data,
-  //         statsDaily: all[1].data,
-  //         dataArrived: true
-  //       }));
-  //   });
-  // }, []);
+  // sets all variables to true to show initially all data on chart
+  const { showImpressions,
+          showRevenue,
+          showClicks,
+          showEvents,
+          showDataTable,
+          hideAddData } = useHideAddData('');
+
+  // tell Buttons to show DataTable button
+  let showDataTableButton = true;
+
+  const SHOWIMPRESSIONS = 'showImpressions';
+  const SHOWREVENUE = 'showRevenue';
+  const SHOWCLICKS = 'showClicks';
+  const SHOWEVENTS = 'showEvents';
+  const SHOWDATATABLE = 'showDataTable';
 
   return (
     <Container>
-        <Row>
+        <Row className='bttn-table'>
           <Col>
-            <Buttons/>
+            <Buttons
+              showDataTableButton={showDataTableButton}
+              onClickImpressions={ () => hideAddData(SHOWIMPRESSIONS) }
+              onClickRevenue={ () => hideAddData(SHOWREVENUE) }
+              onClickClicks={ () => hideAddData(SHOWCLICKS) }
+              onClickEvents={ () => hideAddData(SHOWEVENTS)  }
+              onClickTable={ () => hideAddData(SHOWDATATABLE)  }
+            />
           </Col>
           <Col>
+            { showDataTable &&
+              <GeoDataTable/>
+            }
           </Col>
         </Row>
         <Row>
-          <GeoMapData/>
+          { state.dataArrived &&
+            <GeoMapData poi={state.poi} />
+          }
         </Row>
     </Container>
   )
