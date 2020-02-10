e72a8f35109a1a80df9e4ff1f23ead8c12f67045
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-10 08:24:16 -0500

    cleaned code

--- client/src/hooks/useHideAddData.js
+++ client/src/hooks/useHideAddData.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 /**
  * useHideAddData - deletes or adds data in charts, maps.
@@ -8,47 +8,48 @@ import { useState } from "react";
  * @return {object} - an object which includes the state and two functions.
  */
 const useHideAddData = (initialState, elem) => {
-
-  //set initial visibility of data in charts and map.
+  // set initial visibility of data in charts and map.
   const [state, setState] = useState({
     showRevenue: initialState.revenue,
     showImpressions: initialState.impressions,
     showClicks: initialState.clicks,
     showEvents: initialState.events,
     showLocations: initialState.locations,
-    showDataTable: false,
-  });
+    showDataTable: false
+  })
 
-  console.log('state in hook: ', state);
+  console.log('state in hook: ', state)
 
   /**
    * hideAddData - resets the values of state variables.
    * @param {string} elem - a string equal in value to one of the state keys.
    */
   const hideAddData = (elem) => {
-    if(elem) {
-      state[elem] ?
-        setState({...state, [elem]: false}) :
-        setState({...state, [elem]: true});
+    if (elem) {
+      state[elem]
+        ? setState({ ...state, [elem]: false })
+        : setState({ ...state, [elem]: true })
     }
-  };
+  }
 
- /**
-  * selectDataOnMap - setting the var state to false except the one selected to show data.
-  * @param {String} elem - a string equal in value to one of the state keys.
-  */
+  /**
+    * selectDataOnMap - setting the var state to false except the one selected to show data.
+    * @param {String} elem - a string equal in value to one of the state keys.
+    */
   const selectDataOnMap = (elem) => {
     console.log()
-    //first we hide all data, then we we show only the selected data.
-    setState({showRevenue: false,
-              showImpressions: false,
-              showClicks: false,
-              showEvents: false,
-              showLocations: false,
-              [elem]:true});
-  };
+    // first we hide all data, then we we show only the selected data.
+    setState({
+      showRevenue: false,
+      showImpressions: false,
+      showClicks: false,
+      showEvents: false,
+      showLocations: false,
+      [elem]: true
+    })
+  }
 
-  return { ...state, hideAddData, selectDataOnMap };
+  return { ...state, hideAddData, selectDataOnMap }
 }
 
-export default useHideAddData;
+export default useHideAddData
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faMapMarkerAlt);
+library.add(faMapMarkerAlt)
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'));
+  document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
