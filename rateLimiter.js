fd076953dd59aa6027f5c7104751365966403264
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-10 08:25:36 -0500

    installed eslint

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,45 +1,39 @@
-const moment = require('moment');
-const redis = require('redis');
-
+const redis = require('redis')
 module.exports = (options) => {
+  return function (req, res, next) {
+    // variable to identify user
+    const USER = options.name
+    const client = redis.createClient({
+      port: 6379,
+      host: '127.0.0.1'
+    })
 
-    return function (req, res, next) {
+    client.on('ready', function () {
+      console.log('Redis is ready')
+    })
 
-        //variable to identify user
-        let user = options.name;
-        client = redis.createClient({
-            port: 6379,
-            host: '127.0.0.1'
-        });
+    client.on('error', function () {
+      console.log('Error in Redis')
+    })
 
-        client.on('ready',function() {
-         console.log("Redis is ready");
-        });
-
-        client.on('error',function() {
-         console.log("Error in Redis");
-        });
-
-        //start multi transactions for one request so we avoid
-        //conflict when two requests arrive in the same time
-        client
-            .multi()
-            //sets counter value for user to 0 and expires it in 60 sec
-            .set([user, 0, 'EX', 60, 'NX'])
-            // we increment counter
-            .incr(user) // INCR UUID
-            .exec((err, response) => {
-                if (err) {
-                    return res.status(500).send(err.message)
-                }
-                const reqestCounter = response[1];
-                console.log('user: ', user);
-                console.log('reqCount:', reqestCounter);
-                if (reqestCounter > 20) {
-                    return res.status(429)
-                    .send(`You exceeded your quota of requests per minute! Please try again later!`)
-                }
-                return next();
-            })
-    }
+    // start multi transactions for one request so we avoid
+    // conflict when two requests arrive in the same time
+    client
+      .multi()
+      // sets counter value for user to 0 and expires it in 60 sec
+      .set([USER, 0, 'EX', 60, 'NX'])
+      // we increment counter
+      .incr(USER) // INCR UUID
+      .exec((err, response) => {
+        if (err) {
+          return res.status(500).send(err.message)
+        }
+        const reqestCounter = response[1]
+        if (reqestCounter > 20) {
+          return res.status(429)
+            .send('You exceeded your quota of requests per minute! Please try again later!')
+        }
+        return next()
+      })
+  }
 }
