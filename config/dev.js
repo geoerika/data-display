668c109514ebf19853f299a61df23e3d6b1920fe
Erika Szabo <41120953+geoerika@users.noreply.github.com>
2020-02-10 22:07:12 -0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,4 @@
 module.exports = {
-  PGHOST: 'work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com',
-  PGPORT: 5432,
-  PGDATABASE: 'work_samples',
-  PGUSER: 'readonly',
-  PGPASSWORD: 'w2UIO@#bg532!',
-
   REDISPORT: 6379,
   REDISHOST: '120.0.0.1'
 }
