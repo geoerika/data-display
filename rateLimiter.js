847a661172e24e389ae84abb1dfc0d21faa273a3
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-12 11:34:14 -0500

    inserted a client.quit()

--- rateLimiter.js
+++ rateLimiter.js
@@ -9,29 +9,35 @@ module.exports = (options) => {
       console.log('Redis is ready')
     })
 
-    client.on('error', function () {
+    client.on('error', function (error) {
       console.log('Error in Redis: ', error)
     })
+
+    let redisError = {};
+    let requestCounter = 0;
 
     // start multi transactions for one request so we avoid
     // conflict when two requests arrive in the same time
     client
       .multi()
       // sets request counter value for user to 0 and expires it in 60 sec
-      .set([USER, 0, 'EX', 2, 'NX'])
+      .set([USER, 0, 'EX', 30, 'NX'])
       // we increment counter for user
       .incr(USER)
+      .get(USER, (err, response) => console.log(response))
       .exec((err, response) => {
         if (err) {
           return res.status(500).send(err.message)
         }
         // we read the response value of the nr of requests
         const requestCounter = response[1]
-        if (requestCounter > 20) {
+        if (requestCounter > 6) {
           return res.status(429)
             .send('You exceeded your quota of requests per minute! Please try again later!')
         }
         return next()
       })
+
+      client.quit(() => console.log('quiting redis'))
   }
 }
