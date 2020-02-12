7715268c9b06be65d20e208899ab97a4d5b15f9a
Erika Szabo <41120953+geoerika@users.noreply.github.com>
2020-02-11 23:03:07 -0500

    Delete dev.js

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-//postgres://readonly:w2UIO@#bg532!@work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com:5432/work_samples
-
-module.exports = {
-  DATABASE_URL: 'postgresql://readonly:w2UIO%40%23bg532%21@work-samples-db.cx4wctygygyq.us-east-1.rds.amazonaws.com:5432/work_samples'
-}
