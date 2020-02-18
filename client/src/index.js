405fc10f863c4ee92201478fae3b9b626e5724a0
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-18 15:59:51 -0500

    moved ) on new line

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './index.scss'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './components/custom.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChartBar, faChartArea, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faMapMarkerAlt)
+library.add(faChartBar, faChartArea, faGlobeAmericas)
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'))
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
