afaf688498df38fc373232f146c682590fec4111
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-18 23:27:57 -0500

    added expand='md' to navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -7,14 +7,14 @@ import { Navbar, Nav } from 'react-bootstrap'
 const Navigation = () => {
 
   return (
-    <Navbar bg='dark' variant='dark' className='justify-content-between'>
+    <Navbar bg='dark' variant='dark' expand='md' className='justify-content-between'>
       <Navbar.Brand>Data Display React</Navbar.Brand>
-      <Nav>
-        <Nav.Link href='/'>Home</Nav.Link>
-        <Nav.Link href='/dailydata'>DailyDataChart</Nav.Link>
-        <Nav.Link href='/hourlydata'>HourlyDataChart</Nav.Link>
-        <Nav.Link href='/geodata'>GeoMapData</Nav.Link>
-      </Nav>
+        <Nav>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/dailydata'>DailyDataChart</Nav.Link>
+          <Nav.Link href='/hourlydata'>HourlyDataChart</Nav.Link>
+          <Nav.Link href='/geodata'>GeoMapData</Nav.Link>
+        </Nav>
     </Navbar>
   )
 }
