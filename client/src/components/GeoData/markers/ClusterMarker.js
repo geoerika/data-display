2f4ccb298262c03fd1062f6cdd42f03384188cbe
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-18 22:32:42 -0500

    rewrote code to export ClusterMarker component

--- client/src/components/GeoData/markers/ClusterMarker.js
+++ client/src/components/GeoData/markers/ClusterMarker.js
@@ -1,30 +1,21 @@
 import React from 'react'
-import compose from 'recompose/compose'
-import defaultProps from 'recompose/defaultProps'
-import clusterMarkerStyles from './ClusterMarker.scss'
 import ClusterMarkerSymbol from './ClusterMarkerSymbol.png'
+import './ClusterMarker.scss'
 
-// code below has been modified from https://github.com/istarkov/google-map-clustering-example.
-export const clusterMarker = ({ styles, text }) => (
-  <div className={ styles.marker } >
-    <div className={ styles.text } >
-      { text }
+/**
+ * ClusterMarker - creates ClusterMarker component, with nr of points displayed.
+ * @param {String} text - number of data points in the cluster.
+ * @return {any} - a React component representing a data cluster on the map.
+ */
+const ClusterMarker = ({ text }) => {
+  return (
+    <div className='marker' >
+      <img src={ ClusterMarkerSymbol } alt={ 'ClusterImage' } style={{ width: '100%' }}/>
+      <div className='text'>
+        { text }
+      </div>
     </div>
-    <div>
-      <img src={ ClusterMarkerSymbol }
-        alt= {'https://github.com/googlemaps/v3-utility-library/blob/master/markerclusterer/images/m4.png'}
-      />
-    </div>
-  </div>
-)
+  )
+}
 
-export const clusterMarkerHOC = compose(
-  defaultProps({
-    text: '0',
-    styles: clusterMarkerStyles,
-    initialScale: 0.6,
-    defaultScale: 1
-  })
-)
-
-export default clusterMarkerHOC(clusterMarker)
+export default ClusterMarker
