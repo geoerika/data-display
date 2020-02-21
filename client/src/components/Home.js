8fa5731cbe6106192510f10e5a91210ce91dfd89
Erika Szabo <erika.szabo.geo@gmail.com>
2020-02-21 14:10:59 -0500

    changed Home description

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,12 +8,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Home = () => {
   return (
     <div>
-      <h1 style={ { color: 'red', marginTop: '20vh'} }>Welcome to Erika's solutions</h1>
+      <h1 style={ { color: 'red', marginTop: '20vh'} }>Data Display React</h1>
+      <br/>
       <div>
         <FontAwesomeIcon className='chart-bar' icon='chart-bar' size='3x' style={{ 'marginRight': '10px'}} />
         <FontAwesomeIcon className='chart-area' icon='chart-area' size='3x' style={{ 'marginRight': '10px'}}/>
         <FontAwesomeIcon className='globe-americas' icon='globe-americas' size='3x' />
       </div>
+      <br/>
+      <h3>Simple application to display data in React using charts, tables, and map.</h3>
     </div>
   )
 }
